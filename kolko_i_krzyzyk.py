2c400c001a331b1d3af68403733c6c8c6868a412
Wojciech Handke <handkewojciech@gmail.com>
2018-11-16 00:37:37 +0100

    Added simple line recognition

--- kolko_i_krzyzyk.py
+++ kolko_i_krzyzyk.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 cam = cv2.VideoCapture(0)
 
@@ -8,6 +9,28 @@ img_counter = 0
 
 while True:
     ret, frame = cam.read()
+
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    retTresh, img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+
+    # img = cv2.Canny(img, 50, 150, apertureSize=3)
+
+    lines = cv2.HoughLines(img, 1, np.pi / 180, 200)
+
+    if lines is not None:
+        for line in lines:
+            rho, theta = line[0]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a*rho
+            y0 = b*rho
+            x1 = int(x0 + 1000*(-b))
+            y1 = int(y0 + 1000*(a))
+            x2 = int(x0 - 1000*(-b))
+            y2 = int(y0 - 1000*(a))
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
     cv2.imshow("test", frame)
     if not ret:
         break
